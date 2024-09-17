48e68c82590215b4a905f9fad362ae2c879aee35
intellicatechnlogies <intellicatechnlogies@gmail.com>
2024-09-17 16:18:07 +0530

    login add added

--- IntellicaTechnologies/settings.py
+++ IntellicaTechnologies/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ds-s*%@q!k*+lne!z_aig1a6kqp=^m^8a(ng33(*7=a*(w6a89
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['3.85.212.49','localhost']
+ALLOWED_HOSTS = ['3.85.212.49','localhost','127.0.0.1']
 
 
 # Application definition
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Landing'
+    'Landing',
+    'login'
 ]
 
 MIDDLEWARE = [
